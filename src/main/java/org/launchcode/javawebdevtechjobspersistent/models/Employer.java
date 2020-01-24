eefecc0b03423877e7b458a7c23b9d65dfe0a7cf
imSarahLaunch <syoungae@gmail.com>
2020-01-24 16:47:37 -0600

    finished abstractentity

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 120, message = "Location must be between 3 and 120 characters")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
