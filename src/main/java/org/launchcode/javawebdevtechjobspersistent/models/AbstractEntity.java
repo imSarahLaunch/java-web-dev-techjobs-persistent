721dfa254cce56bfe2c3c74651ef3562336e1f8f
imSarahLaunch <syoungae@gmail.com>
2020-01-24 10:41:03 -0600

    finished part1 connect a database to a spring app

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,8 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
     private int id;
